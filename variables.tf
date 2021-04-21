5946a5f7a6209126b52970a03bdadf1f515ebf77
Sean Pascual <sean.pascual@umotif.com>
2021-04-21 12:09:47 +0100

    Readme update

--- variables.tf
+++ variables.tf
@@ -117,13 +117,13 @@ variable "create_repository_credentials_iam_policy" {
 }
 
 variable "placement_constraints" {
-  type        = list
+  type        = list(any)
   description = "(Optional) A set of placement constraints rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. This is a list of maps, where each map should contain \"type\" and \"expression\""
   default     = []
 }
 
 variable "proxy_configuration" {
-  type        = list
+  type        = list(any)
   description = "(Optional) The proxy configuration details for the App Mesh proxy. This is a list of maps, where each map should contain \"container_name\", \"properties\" and \"type\""
   default     = []
 }
